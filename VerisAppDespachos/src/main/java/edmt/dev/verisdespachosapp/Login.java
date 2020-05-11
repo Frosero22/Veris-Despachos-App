6c5a8ec641132f3feff4c04d32c1ee7594b90380
Francesco <francesco.villacis@gmail.com>
2020-05-11 16:42:56 -0500

    Generador de Token Incorporado Validador de Rol Incorporado Servicio Login Incorporado

--- VerisAppDespachos/src/main/java/edmt/dev/verisdespachosapp/Login.java
+++ VerisAppDespachos/src/main/java/edmt/dev/verisdespachosapp/Login.java
@@ -7,9 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -25,27 +30,36 @@ import okhttp3.ResponseBody;
 
 public class Login extends AppCompatActivity {
 Button LoginD;
+String Token;
+EditText User;
+EditText Pass;
+    Boolean Validador = false;
 private static ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+         User = findViewById(R.id.edit_user);
+         Pass = findViewById(R.id.edit_pass);
 
         LoginD = findViewById(R.id.btn_login);
         LoginD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // RecuperaToken();
 
-               Intent intent = new Intent(Login.this,Pantalla_Principal.class);
-                startActivity(intent);
+
+
+                    RecuperaToken();
+
+
             }
         });
+
+
+
     }
-
-
-
-    public void RecuperaToken(){
+//TOKEN NECESARIO PARA TODOS LOS SERVICIOS RESTANTES
+    public String RecuperaToken(){
 
         OkHttpClient client = new OkHttpClient();
         JsonObject postData = new JsonObject();
@@ -76,8 +90,12 @@ private static ProgressDialog progressDialog;
 
                         throw new IOException("Error Inesperado " + response);
                     }
+                    assert responseBody != null;
+                    JSONObject object = new JSONObject(response.body().string());
+                    Log.e("Token" ,"Token ->" + object.getString("accesToken"));
+                    Token = object.getString("accesToken");
+                 LoginConToken(Token);
 
-                    Log.i("data", responseBody.string());
                     Log.e("Ok","Token Generado");
                 } catch (Exception e) {
 
@@ -87,6 +105,97 @@ private static ProgressDialog progressDialog;
             }
         });
 
+            return Token;
     }
 
-}
+
+    public void LoginConToken(String Token) throws JSONException {
+
+
+
+          Log.e("Token Recogido --->","Token   " + Token);
+
+
+
+        OkHttpClient client = new OkHttpClient();
+        JSONObject postData = new JSONObject();
+
+        postData.put("user",User.getText().toString().trim());
+        postData.put("pass",Pass.getText().toString().trim());
+
+        Log.e("Usuario","User es = " +User.getText().toString());
+        Log.e("Pass","Pass es = " +Pass.getText().toString());
+
+
+        final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody postBody = RequestBody.create(JSON, postData.toString());
+        Request post = new Request.Builder()
+                .url("http://52.7.160.244:8223/Verisrest/v1/formularioepi1/loginUser")
+                .post(postBody)
+
+                .addHeader("Authorization", "Bearer "+Token)
+                .build();
+
+        client.newCall(post).enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("Error","Error al ejecutarr servicio" +e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                try {
+                    ResponseBody responseBody = response.body();
+                    if (!response.isSuccessful()) {
+
+                        throw new IOException("Error Inesperado " + response);
+                    }
+
+                    assert responseBody != null;
+
+                    JSONObject jsonObject = new JSONObject(responseBody.string());
+                    String listaXrol = jsonObject.getString("lsUsuarioXRol");
+                    JSONArray jsonArray = new JSONArray(listaXrol);
+
+                        for(int a = 0; a < jsonArray.length(); a++){
+                            JSONObject js = jsonArray.getJSONObject(a);
+                            if(js.getString("codigoRol").equals("DESPACHO_FARMACIA")
+                                    && js.getString("codigoUsuario").equals(User)){
+                                    Validador = true;
+
+                            }else{
+                                Validador = false;
+                            }
+                        }
+
+                            if(Validador = true){
+                                Log.e("Ok","Acceso Listo");
+
+                                Intent intent = new Intent(Login.this,Pantalla_Principal.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(Login.this, "No Contiene el Rol Correcto", Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                    Log.e("Error","Error--->"+e);
+                }
+            }
+            });
+
+
+            }
+        }
+
+
+
+
--- VerisAppDespachos/src/main/java/edmt/dev/verisdespachosapp/Pantalla_Principal.java
+++ VerisAppDespachos/src/main/java/edmt/dev/verisdespachosapp/Pantalla_Principal.java
@@ -1,13 +1,24 @@
 package edmt.dev.verisdespachosapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.zxing.Result;
 import com.google.zxing.integration.android.IntentIntegrator;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
 
 public class Pantalla_Principal extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
@@ -24,15 +35,7 @@ public class Pantalla_Principal extends AppCompatActivity implements ZXingScanne
             @Override
             public void onClick(View view) {
 
-                IntentIntegrator integrator = new IntentIntegrator(Pantalla_Principal.this);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
-                integrator.setPrompt("Escanea el Codigo QR de la Factura");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(false);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
-
-
+                        Dialogo();
 
 
             }
@@ -43,7 +46,87 @@ public class Pantalla_Principal extends AppCompatActivity implements ZXingScanne
     @Override
     public void handleResult(Result result) {
 
+
+
+
+
     }
+
+
+    private void Dialogo() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Pantalla_Principal.this);
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialogo_opcion, null);
+
+        builder.setView(view);
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        Button BotonIngresarId = view.findViewById(R.id.btn_id);
+        Button BotonLector = view.findViewById(R.id.btn_lector);
+        BotonIngresarId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Pantalla_Principal.this);
+
+                LayoutInflater inflater = getLayoutInflater();
+
+                View view = inflater.inflate(R.layout.dialog_personalizado, null);
+
+                builder.setView(view);
+
+
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+
+                final EditText Id = view.findViewById(R.id.edit_id);
+                Button BotonId = view.findViewById(R.id.btn_procesar);
+                BotonId.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                            String Solicitud = Id.getText().toString().trim();
+                    }
+                });
+
+                Button BotonCancelar = view.findViewById(R.id.btn_cancelar_pick);
+                BotonCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                //SERVICIO REGISTRAR PICK
+
+
+
+
+            }
+        });
+
+        BotonLector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                IntentIntegrator integrator = new IntentIntegrator(Pantalla_Principal.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+                integrator.setPrompt("Escanea el Codigo Qr");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
+
+
+            }
+
+        });
+
+    }
+
 }
 
 
